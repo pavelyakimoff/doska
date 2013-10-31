70893e31872fc2eecb7c06c1f66a2da950cf7f59
Pavel Yakimov <yakimoff@lenta-co.ru>
2013-10-31 12:58:47 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doska::Application.config.secret_key_base = '50fa84631db03cc90488d1ee2d1446a627f459c6c032b39f1974c41ed9bea1cb7fb0e68352fd9fffa69080f13332bcabb03dc9f187045b9e58fdf3bb97317f6b'
